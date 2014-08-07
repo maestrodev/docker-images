03f753d26f1e013f9d91e3244d5b1614e3f9a518
Carlos Sanchez <csanchez@maestrodev.com>
2014-08-07 13:59:33 +0200

    Delete maestro-agent docker image

--- maestro-agent/agent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ruby /agent.rb $STOMP_HOST $STOMP_USER $STOMP_PASSCODE
-/sbin/service maestro-agent console && /bin/sh /cmd.sh
--- maestro-agent/site.pp
+++
@@ -1,7 +0,0 @@
-node 'default' {
-
-  include maestro::params
-
-  class { 'maestro_nodes::agent::rvm': }
-  class { 'maestro::agent': }
-}
